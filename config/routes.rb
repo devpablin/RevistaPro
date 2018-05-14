093e6114e3dd44e3271b43174faea1c3af33cf73
DevPablin <pablingonzales@gmail.com>
2018-05-13 21:28:29 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  resources :events
+  # resources :events
   root 'welcome#building'
-  resources :artists
-  resources :videos
-  resources :reviews
-  resources :reports, path: :news
-  resources :categories
+  resources :artists, only: [:new, :edit, :create, :destroy, :update]
+  # resources :videos
+  resources :reviews, only: [:show, :edit, :create, :destroy, :update]
+  # resources :reports, path: :news
+  # resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
