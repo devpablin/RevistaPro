72b8629d8e05dc2501f14e3c0c63fbeb66042bac
DevPablin <pablingonzales@gmail.com>
2018-05-15 19:20:19 -0400

    routes news

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :artists, only: [:new, :edit, :create, :destroy, :update]
   # resources :videos
   resources :reviews, only: [:show, :edit, :create, :destroy, :update]
-  # resources :reports, path: :news
+  resources :reports, path: :news
   # resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
