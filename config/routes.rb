f1323767e60fbb5b3e43aa289e63b46a6107bb20
DevPablin <pablingonzales@gmail.com>
2018-05-20 20:56:25 -0400

    2nd try for mobile hover effect on reviews

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#building'
   resources :artists, only: [:new, :edit, :create, :destroy, :update]
   # resources :videos
-  resources :reviews, only: [:show, :edit, :create, :destroy, :update, :new]
+  resources :reviews
   resources :reports, path: :news
   # resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
